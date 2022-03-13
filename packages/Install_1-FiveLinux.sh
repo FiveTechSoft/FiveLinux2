f3b63f4f296cbd12cc430b3838f8d8df53ade1f8
AntonioFS <91391996+AntonioFS-1962@users.noreply.github.com>
2022-03-13 14:52:09 +0100

    Add files via upload

---
+++ packages/Install_1-FiveLinux.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt upgrade
+sudo apt update
+sudo apt install -y libgtk2.0-dev
+sudo apt install -y libgtk-3-dev
+sudo apt install -y glade
+sudo apt install -y libglade2-0
+sudo apt install -y libglade2-dev
+git clone https://github.com/FiveTechSoft/fivelinux.git
---
+++ packages/Install_3-FiveLinux.sh
@@ -0,0 +1,6 @@
+sudo apt update
+cd fivelinux
+make
+sudo apt update
+cd samples
+./build.sh tutor01
