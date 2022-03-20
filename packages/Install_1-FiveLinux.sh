748cfc0a1ff6a277d586ed056a94428fb7e9457c
AntonioFS <91391996+AntonioFS-1962@users.noreply.github.com>
2022-03-20 23:07:42 +0100

    Add files via upload

--- packages/Install_1-FiveLinux.sh
+++ packages/Install_1-FiveLinux.sh
@@ -7,6 +7,8 @@ sudo apt install -y libgtk-3-dev
 sudo apt install -y glade
 sudo apt install -y libglade2-0
 sudo apt install -y libglade2-dev
+sudo apt install openssl -y
+sudo apt install curl -y
 sudo apt install -y gpm
 sudo apt install -y gcc
 sudo apt install -y git
